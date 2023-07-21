6ab3c40b879c6c9fb5b3c62c1d41f086e85febef
Stephen <venter76@gmail.com>
2023-07-21 14:07:39 +0200

    2

--- createPDF.js
+++ createPDF.js
@@ -4,8 +4,9 @@ async function createPDF(url) {
   try {
     const browser = await puppeteer.launch({
       headless: true,
+      executablePath: process.env.CHROME_PATH,
       // Use the correct environment variable for your Chrome or Chromium path
-      executablePath: process.env.LOCAL_CHROME_PATH,
+    //   executablePath: process.env.LOCAL_CHROME_PATH,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
